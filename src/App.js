8007f9dabfddf3b12fa9db55be12299184c63637
lucas <mr.pine97@gmail.com>
2021-12-10 21:08:29 -0300

    adicionando hashrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import RoutesConfigs from './RoutesConfigs';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <RoutesConfigs />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
